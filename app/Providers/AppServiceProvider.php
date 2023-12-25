8b7ee7026ab906e6f26da0684f97061614e747e3
a7medKhalid <ahmed1alghamdi2000@gmail.com>
2023-12-25 22:35:18 +0300

    created files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //unguarding models
+        \Illuminate\Database\Eloquent\Model::unguard();
     }
 }
