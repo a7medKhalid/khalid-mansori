4421637fb23e1be11878f6857da4751d1f85fc28
a7medKhalid <ahmed1alghamdi2000@gmail.com>
2023-12-27 21:53:33 +0300

    idn

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $articles = App\Models\Article::all();
+    return view('welcome', ['articles' => $articles]);
 });
