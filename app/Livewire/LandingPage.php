5f7d1e6b5b2dc5b348365f1f01bc1811aea73f26
a7medKhalid <ahmed1alghamdi2000@gmail.com>
2024-09-06 18:05:04 +0300

    dynamic

--- app/Livewire/LandingPage.php
+++ app/Livewire/LandingPage.php
@@ -2,20 +2,26 @@
 
 namespace App\Livewire;
 
+use App\Models\Expert;
+use App\Models\Goal;
 use App\Models\Hero;
-use App\Models\User;
+use App\Models\ServiceCategory;
 use Livewire\Component;
 
 class LandingPage extends Component
 {
     public Hero $hero;
     public array $goals;
-    public User $leader;
+    public Expert $leader;
     public array $experts;
     public array $servicesCategories;
 
     public function mount(){
         $this->hero = Hero::inRandomOrder()->first();
+        $this->goals = Goal::orderBy('order')->get()->toArray();
+        $this->leader = Expert::where('isLeader', true)->first();
+        $this->experts = Expert::where('isLeader', false)->orderBy('order')->get()->toArray();
+        $this->servicesCategories = ServiceCategory::with('services')->orderBy('order')->get()->toArray();
     }
 
     public function render()
--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -5,15 +5,14 @@
     <div class="flex h-screen w-screen items-center">
         <div class="w-full pr-8">
             <h3 class="">
-                ميادير للتنمية والابتكار
+                {{ $hero->title }}
             </h3>
             <h1 class="text-6xl font-bold">
-                تصميم الدراسات والمبادرات بإطار
-                منهجي وأساليب مبتكرة
+                {{ $hero->description }}
             </h1>
         </div>
         <div class="w-2/3 flex justify-center items-center">
-            <img class="max-w-full h-auto object-cover rounded-lg shadow-lg" src="images/logo.jpg" alt="Logo" />
+            <img class="max-w-full h-auto object-cover rounded-lg shadow-lg" src="{{ $hero->image_path }}" alt="Logo" />
         </div>
     </div>
 
@@ -22,33 +21,18 @@
         <h2 class="text-4xl ">
             أهدافنا
         </h2>
-        <div class="flex justify-center items-center">
-            <div class="p-4">
-                @for($i = 0; $i < 3; $i++)
-                    {{-- Goal --}}
-                    <div class="flex items-center pt-16 ">
-                        <p class="text-8xl ml-4">
-                            {{ $i + 1 }}
-                        </p>
-                        <p class="text-4xl">
-                            إعداد الدراسات العلمية، والمقاييس التربوية، والنفسية، وتحكيمها
-                        </p>
-                    </div>
-                @endfor
-            </div>
-            <div class="p-4">
-                @for($i = 3; $i < 5; $i++)
-                    {{-- Goal --}}
-                    <div class="flex items-center pt-16 ">
-                        <p class="text-8xl ml-4">
-                            {{ $i + 1 }}
-                        </p>
-                        <p class="text-4xl">
-                            إعداد الدراسات العلمية، والمقاييس التربوية، والنفسية، وتحكيمها
-                        </p>
-                    </div>
-                @endfor
-            </div>
+        <div class="grid grid-cols-2 items-center">
+            @foreach($goals as $goal)
+                {{-- Goal --}}
+                <div class="flex items-center pt-16 ">
+                    <p class="text-8xl ml-4">
+                        {{ $goal['order'] }}
+                    </p>
+                    <p class="text-4xl">
+                        {{ $goal['title'] }}
+                    </p>
+                </div>
+            @endforeach
         </div>
     </div>
 
@@ -58,11 +42,11 @@
             رئيس بيت الخبرة
         </h2>
         <div class="mt-12 flex">
-            <img class="w-1/3" src="images/leader.png"  alt=""/>
+            <img class="w-1/3" src="{{ $leader->image_path }}"  alt=""/>
             <div class="mt-24">
-                <h3 class="font-bold text-6xl">د. خالد المنصوري </h3>
+                <h3 class="font-bold text-6xl"> {{ $leader->name }} </h3>
                 <p class="text-2xl mt-8">
-                    بين التعليم والتطوير ومن حجرة الدراسة إلى ساحات المجتمع تتحرك قدماي وتتنقل أفكاري وتنمو مشاريعي نحو استدامة مجتمعية ترتقي بحياتنا في الحاضر وتحقق لنا التمكين في المستقبل إن على ظاهر الأرض أو في باطنها 
+                    {{ $leader->description }}
                 </p>
             </div>
         </div>
@@ -74,46 +58,20 @@
         <h2 class="text-4xl ">
             خبراء ميادير
         </h2>
-        <div class="grid grid-cols-4 mt-12">
-            <div class="flex flex-col items-center mt-12 ">
-                <img class="w-2/3" src="images/leader.png"  alt=""/>
-                <div class="text-center mt-12">
-                    <h3 class="font-bold text-4xl">د. خالد المنصوري </h3>
-                    <p class="text-2xl mt-8">
-                       تقي بحياتنا في الحاضر وتحقق لنا التمكين في المستقبل إن على ظاهر الأرض أو في باطنها 
-                    </p>
+        <div class="grid grid-cols-3 mt-12">
+            @foreach($experts as $expert)
+                <div class="flex flex-col items-center mt-12 ">
+                    <img class="w-2/3" src="{{ $expert['image_path'] }}"  alt=""/>
+                    <div class="text-center mt-12">
+                        <h3 class="font-bold text-4xl">
+                            {{ $expert['name'] }}
+                        </h3>
+                        <p class="text-2xl mt-8">
+                            {{ $expert['description'] }}
+                        </p>
+                    </div>
                 </div>
-            </div>
-
-            <div class="flex flex-col items-center mt-12 ">
-                <img class="w-2/3" src="images/leader.png"  alt=""/>
-                <div class="text-center mt-12">
-                    <h3 class="font-bold text-4xl">د. خالد المنصوري </h3>
-                    <p class="text-2xl mt-8">
-                        بين التعليم والتطوير ومن حجرة الدراسة إلى ساحات المجتمع تتحرك قدماي وتتنقل أفكاري وتنمو مشاريعي نحو استدامة مجت
-                    </p>
-                </div>
-            </div>
-
-            <div class="flex flex-col items-center mt-12 ">
-                <img class="w-2/3" src="images/leader.png"  alt=""/>
-                <div class="text-center mt-12">
-                    <h3 class="font-bold text-4xl">د. خالد المنصوري </h3>
-                    <p class="text-2xl mt-8">
-                        بين التعليم والتطوير ومن حجرة الدراسة إلى ساحات العلى ظاهر الأرض أو في باطنها 
-                    </p>
-                </div>
-            </div>
-
-            <div class="flex flex-col items-center mt-12 ">
-                <img class="w-2/3" src="images/leader.png"  alt=""/>
-                <div class="text-center mt-12">
-                    <h3 class="font-bold text-4xl">د. خالد المنصوري </h3>
-                    <p class="text-2xl mt-8">
-                        بين التعليم  التمكين في المستقبل إن على ظاهر الأرض أو في باطنها 
-                    </p>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 
@@ -123,35 +81,23 @@
             خدماتنا
         </h2>
         <div class="mt-12 ">
-            <div>
-                <p class="w-1/4 border-b-4 text-2xl font-bold"> إعداد وتصميم</p>
-                <div class="grid grid-cols-4 mt-4">
-                   <div class="">
-                       <img src="images/service.png" alt="" />
-                       <p>
-                           الدراسات العلمية
-                       </p>
-                   </div>
-                    <div>
-                        <img src="images/service.png" alt="" />
-                        <p>
-                            الدراسات العلمية
-                        </p>
+           @foreach($servicesCategories as $servicesCategory)
+                <div>
+                    <p class="w-1/4 border-b-4 text-2xl font-bold">
+                        {{ $servicesCategory['title'] }}
+                    </p>
+                    <div class="grid grid-cols-4 mt-4">
+                        @foreach($servicesCategory['services'] as $service)
+                            <div class="">
+                                <img src="images/service.png" alt="" />
+                                <p>
+                                   {{ $service['title'] }}
+                                </p>
+                            </div>
+                        @endforeach
                     </div>
-                    <div>
-                        <img src="images/service.png" alt="" />
-                        <p>
-                            الدراسات العلمية
-                        </p>
-                    </div>
-                    <div>
-                        <img src="images/service.png" alt="" />
-                        <p>
-                            الدراسات العلمية
-                        </p>
-                    </div>
-               </div>
-            </div>
+                </div>
+           @endforeach
         </div>
     </div>
 
